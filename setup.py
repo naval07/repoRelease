73a66eb36fc5e281a2e2897a4a04092eb1158e62
Emanuel <emanuelnaval07@gmail.com>
2025-03-19 23:42:09 -0500

    primer lanzamiento de la app

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="repoRelease",                             # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Emanuel Naval",                         # Tu nombre
+    author_email="emanuelnaval07@gmail.com",        # Tu correo electrónico
+    url="https://github.com/naval07/repoRelease",   # URL del proyecto
+)
